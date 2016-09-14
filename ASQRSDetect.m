d43870dfca50d36c64892ba9480344e6db450a41
Ashwin Sundar <ashiundar@gmail.com>
2016-09-13 20:51:18 -0700

    Added R Peak detection
    Added R Peak detection. Reorganized variable declaration to improve code
    modularity.

--- ASQRSDetect.m
+++ ASQRSDetect.m
@@ -6,8 +6,7 @@
 % Name of Editor: Ashwin Sundar
 % Date of GitHub commit: September 11, 2016
 % What specific changes were made to this code, compared to the currently 
-% up-to-date code on GitHub?: Retrieved data, calculated statistics,
-% plotted it. 
+% up-to-date code on GitHub?: Added R Peak detection
 %%%% %%%% %%%% %%%%
 % Best coding practices
 % 1) When you create a new variable or function, make it obvious what the 
@@ -20,10 +19,11 @@
 % committing changes to GitHub.
 %%%% %%%% %%%% %%%% 
 %%%% %%%% %%%% %%%% 
-buffer = zeros(100, 2); % circular buffer containing last 100 data points 
-% (0.5 seconds @ 200Hz). I chose this number because the max string length
-% I can make in Particle is 623 chars, each data point is (up to) 6 digits
-% long, 623/6 ~ 100 data points stored in circular buffer. 
+sampleRate = 360; % Sample rate of MIT-BIH Arrhythmia data
+QRSLength = 150; % QRS complex spans 60-100 ms in healthy patients, up to 
+% 150 ms in patients with cardiac problems (Sivaraks 2014, Kim 2016). 
+buffer = zeros(sampleRate*QRSLength/1000, 2); % circular buffer containing 
+% last 54 data points (150ms).  
 signalMean = 0; % average of circular buffer. Can be implemented in multiple 
 % ways. Run perf testing to determine best technique. 
 % Perf Test 1: Use inbuilt mean(data) function in MATLAB. Log results
@@ -50,7 +50,8 @@ cd C:\Users\Ashwin\Dropbox\'Applied Project'\'MIT-BIH Arrhythmia Data'\wfdb-app-
 % resolution of 13 bit. 
 % high resolution: default is off, 1 causes record to be "read in high
 % resolution mode" 
-[tm, signal] = rdsamp('mitdb/100', [], 100, 1, 3);
+
+[tm, signal] = rdsamp('mitdb/100', [], length(buffer), 1, 3);
 buffer(1:end,1) = tm;
 buffer(1:end,2) = signal(1:end,1);
 
@@ -63,4 +64,27 @@ plot(tm, signal(1:end,1));
 hold on;
 plot([0,100],[signalMean,signalMean]);
 plot([0,100],[signalMean+signalStDev,signalMean+signalStDev]);
+plot([0,100],[signalMean+3*signalStDev,signalMean+3*signalStDev]);
 
+% Next, let's locate the R peak. The R peak should be above the signal
+% mean, let's say 3 standard deviations beyond the signalMean. I could
+% preallocate a buffer to store the RPeak, but it's so small that I don't
+% think it will matter. I could use parallel processing, but I won't have 
+% access to that in Particle, so I won't. 
+j = 1; 
+for i=1:(length(buffer)-1)
+    if(buffer(i,2) > signalMean + 3*signalStDev)
+        RPeak(j,:) = buffer(i,:);
+        j = j + 1;
+    end
+end
+
+% I won't have access to a linear algebra toolbox in Particle. Since the
+% amount of data above 3SD is very small, I think I'll just use the median
+% (or left of median if I have an even number of points) 
+if(exist('RPeak'))
+    fprintf('Found R Peak.\n')
+    RPeak = RPeak(floor(length(RPeak)/2),:)
+else
+    fprintf('Did not find R Peak.\n')
+end
