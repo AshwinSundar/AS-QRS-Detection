9c94f2364ec77eb08c2564a5a2655beaa89f5eec
Ashwin Sundar <ashiundar@gmail.com>
2016-09-24 09:08:28 -0700

    C++ changes: Code compiles
    Using std::sscanf() instead of std::stod() - sscanf is native to C, so
    I (meaning Prof Spano) had to do some minor legwork to change
    std::string tempString to a C-string. Code finally compiles and seems
    to work properly!

--- ASQRSDetect/ASQRSDetect/main.cpp
+++ ASQRSDetect/ASQRSDetect/main.cpp
@@ -3,9 +3,9 @@
 //// //// //// //// ////
 // Title of File: main.cpp
 // Name of Editor: Ashwin Sundar
-// Date of GitHub commit: September 21, 2016
+// Date of GitHub commit: September 24, 2016
 // What specific changes were made to this code, compared to the currently up-to-date code
-// on GitHub?: std::stol/stoi/stod (string to double converter) seems to have given up working. I have a feeling I'm running into array indexing errors. I'll look into it tomorrow.
+// on GitHub?: Using std::sscanf() instead of std::stod() - sscanf is native to C, so I (meaning Prof Spano) had to do some minor legwork to change std::string tempString to a C-string. Code finally compiles and seems to work properly!
 //// //// //// //// ////
 // Best coding practices
 // 1) When you create a new variable or function, make it obvious what the variable or
@@ -22,39 +22,50 @@
 #include <cstdlib>
 #include <climits>
 #include <array>
+#include "stdio.h"
 
 std::ifstream myFile;
 std::string line;
-double EKGData[650000][2]; // most MIT BIH data seems to be 650k samples long. I'm not sure why, but if I declare this inside main, I get what appears to be a memory address error. Clearing up space on my HDD didn't affect anything, but reducing the size of the array allocation inside the function, or just putting the declaration outside the function like I did here seems to clear the error.
+double EKGData[650000][2]; // most MIT BIH data seems to be 650k samples long. I'm not sure why, but if I declare this inside main, I get what appears to be a memory address error. Clearing up space on my HDD didn't affect anything, but reducing the size of the array allocation inside the function, or just putting the declaration outside the function like I did here seems to clear the error. I'm declaring this as a double because according to Prof Spano, most computers do their math in double by default. Even if you declare this as a float, which is smaller, your computer will first convert it to a double, do the math, then go back to float - which ends up taking longer.
 
 int main(int argc, const char * argv[])
 {
     std::string inputFile = "MITBIH100fullData.txt"; // I'm doing all this nasty file delimiting in main and not a separate function because I can't return arrays in C++ without some ridiculous legwork.
-    std::string tempString; // holds a number while file is being parsed
+    std::string tempString; // holds a 'number' while file is being parsed
     myFile.open(inputFile);
     
     if (myFile.is_open())
     {
-
-        int j = 1; // used to track columns in EKGData array
+        int j = 0; // used to track columns in EKGData array
+        int k = 0; // used to track rows in EKGData array
         while (getline(myFile, line)) // while the file has more content
         {
-            for(int i = 1; i < line.size(); i++)
+            j = 0;
+            for(int i = 0; i < line.size(); i++)
             {
-                std::cout << line[0];
-                j = 1;
-                switch (line[i] == ',')
-                { // manual delimiting
+                // bool eval = (line[i] == ','); // for debugging
+                // bool eval2 = (i == line.size() - 1); // for debugging
+                switch (line[i] == ',' || i == line.size() - 1) // manual delimiting
+                {
                     case 0: // no comma, keep writing
+                        std::cout << line[i];
                         tempString.push_back(line[i]);
-                    case 1: // comma present, do something
-                        tempString = std::stod(tempString); // write the temp string
+                        break; // switch cases have to break in C++ 
+                    case 1: // comma present, write the first set of data
+                        //std::sscanf(tempString.data(), "%f", &(EKGData[i][j])); // write the temp string
+                        //std::sscanf(tempString.c_str(), "%f", &(EKGData[i][j])); // write the temp string
+                        std::sscanf(tempString.data(), "%lf", &(EKGData[k][j])); // write the temp string
                         tempString = ""; // clear the temp string
                         j++; // move to next column
+                        break;
+                    default:
+                        break;
                 }
             }
+            k++;
         }
     }
     myFile.close();
+    std::cout << EKGData;
     return 0;
 }
